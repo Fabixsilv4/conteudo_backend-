daa7ba3a086fa9e97dd025d16bfeb29139e0e1ce
Fabixsilv4 <125364636+Fabixsilv4@users.noreply.github.com>
2023-03-15 11:28:49 -0300

    aula 9
    tabelas, vetores e afins

---
+++ aula9/produtos.php
@@ -0,0 +1,17 @@
+<?php
+    $produto1 = ["nome" => "Cãmera",
+                "marca" => "LG",
+                "preco" => 2500];
+                
+    
+     $produto2 = ["nome" => "notebook",
+                "marca" => "dell",
+                "preco" => 7500];
+
+    $produto3 = ["nome" => "smartphone",
+                "marca" => "Sansung",
+                "preco" => 3500] ;
+            
+$produtos = [$produto1, $produto2, $produto3] ;
+
+echo json_encode($produtos);
