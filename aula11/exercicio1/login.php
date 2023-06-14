f60fac8cd688c4ff01cddf712af213938c18bd64
Fabixsilv4 <125364636+Fabixsilv4@users.noreply.github.com>
2023-06-14 10:16:23 -0300

    Add files via upload
    aula11

---
+++ aula11/exercicio1/login.php
@@ -0,0 +1,14 @@
+<h1>Restrito</h1>
+
+<?php
+
+$resposta1 = $_GET["n1"];
+$resposta2 = $_GET["n2"];
+
+if($resposta1 == "admin" && $resposta2==123){
+    echo "logado";
+}else{
+    echo "senha ou usuario incorreto";
+}
+
+?>
